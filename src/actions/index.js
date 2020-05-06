02296fcf9b8669c27c28ef4f27cdaeea989dae79
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-05-06 17:45:48 +0200

    Added axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,11 @@
 const API_KEY = process.env.API_KEY;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
+export const FETCH_WEATHER = "FETCH_WEATHER"
+
+
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER
+  }
+}
