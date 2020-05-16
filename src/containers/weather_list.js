9b530b0660d624047c1c8523097a66c8f9d9d0fe
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-05-16 13:47:38 +0200

    Added react sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    console.log(temps)
+    return (
+      <tr key={name}>
+        <td>
+          {name}
+        </td>
+      </tr>
+    )
+
+  }
+
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
 
@@ -18,3 +37,11 @@ export default class WeatherList extends Component {
     )
   }
 }
+
+function mapStateToProps({ weather }) {
+  //const weather = state.weather => the same as  { weather }
+  return { weather }; // {weather} === { weather: weather } 
+
+}
+
+export default connect(mapStateToProps)(WeatherList)
