7681be0f1b289675b74524db9986fd8bdd4c0dd4
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-05-17 11:55:00 +0200

    Completed Table Display

--- src/components/chart.js
+++ src/components/chart.js
@@ -3,17 +3,27 @@
 // Since we do not need to make any use of state inside of our chart 
 // component we can get with functional component.
 // it will just receive prop
-
+//import _from "lodash"
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
+function average(data) {
+
+  const average = data.reduce(function (sum, value) {
+    return sum + value;
+  }, 0) / data.length;
+
+  return average.toFixed(0);
+}
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={100} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
     </div>
   )
 }
---
+++ src/components/google_map.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class GoogleMap extends Component {
+
+
+
+}
+
+
+export default GoogleMap;
